50d46ad3ac98f679a922dceba3f5534fb35cd880
ivancica91 <a.ivancic91@gmail.com>
2020-12-07 22:16:15 +0100

    End of section 7 - errors

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -1,6 +1,7 @@
 using DatingApp.API.Data;
 using DatingApp.API.Extensions;
 using DatingApp.API.Interfaces;
+using DatingApp.API.Middleware;
 using DatingApp.API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
@@ -39,10 +40,10 @@ namespace DatingApp.API
             services.AddApplicationServices(_config);
             services.AddCors();
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DatingApp.API", Version = "v1" });
-            });
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "DatingApp.API", Version = "v1" });
+            //});
             services.AddIdentityServices(_config);
 
         }
@@ -50,12 +51,7 @@ namespace DatingApp.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DatingApp.API v1"));
-            }
+            app.UseMiddleware<ExceptionMiddleware>();
 
             app.UseHttpsRedirection();
 
